b67c97a1ff073f77e7ad41b085736e743842e89a
Rosa <rosa.morales.rojas@gmail.com>
2018-07-06 00:47:14 -0400

    Se incorpora el like en los posts

--- assets/js/lib/script.js
+++ assets/js/lib/script.js
@@ -4,4 +4,11 @@ $( document ).ready(function() {
 	$(this).toggleClass("card--open");
 
 	});
+
+	$(".card__like").on("click", function(event){
+		event.preventDefault();
+	$(this).toggleClass("card__like--red");
+
+ 	});
+
 });
