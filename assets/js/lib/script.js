f1681270bd4a902bbd8ccffdd1d8db18037f3bba
Rosa <rosa.morales.rojas@gmail.com>
2018-07-31 18:25:38 -0400

    update

--- assets/js/lib/script.js
+++ assets/js/lib/script.js
@@ -14,7 +14,19 @@ $( document ).ready(function() {
  	$(".card__follow-btn").on("click", function(event){
 		event.preventDefault();
 	$(this).toggleClass("card__follow-btn--following");
+	 
+	$("#image").change(function(evt){
+	 var image='assets/images/squared/'+$(this).val();
+	$('.create__image .create__img').attr('src', image);
+		//console.log("change" , $(this).val());
+	 });
 
- 	});
+  $("#author").change(function(evt){
+    var image='assets/images/profiles/'+$(this).val();
+  $('.create__profile .create__img').attr('src', image);
+    //console.log("change" , $(this).val());
 
+  });
+
+	});
 });
